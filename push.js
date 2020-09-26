45d91468ad863f631227f07455362e3255ca09c5
jean870809 <61371797+jean870809@users.noreply.github.com>
2020-09-26 11:36:08 +0800

    AAAAA

--- push.js
+++ push.js
@@ -3,10 +3,10 @@
 
 
 var key = 'AAAAsbNDVbs:APA91bG7xWF5l0EUl6UHLeOJyXUtL7AgzmGkw_F4BCoAxKJOZh_SVp7zhfypLaisu2_godUq31RWfTHiu-lzXkIsgvbrmWUBiPlWEDiRCZiPvieZjeiWVp823py053b2z5uJJpUkwJne';
-var to = "dyRFecNPWCXsZQbbbraxwf:APA91bHd2-1TkXATNyXOa3GnMXud6gX4Ie5DKKcSs2Yqh_kKCPpnoU0TLA1anKPNhM2ez4GvbNoe_TpmffvnODGJF2p087dNDh8fvdTJejM6a0qBR5NLzQZB3aQ6wexb9EbG_7rJfI7S";
+var to = "e-nOK15Pd0LNM5ySXhjiSu:APA91bFpPkhgluLXKBBweGabTj_ahuVGfJESTgxbwUvU3umz4u3mS0tAYEUv-kUdNsDzV3ljIqMN3y1Nk4DSeKAWAutbSvI53a0eR7U0F6jQfwZBKWl1ePpAKpMHtK5JO0AOgZsom0kB";
 var notification = {
   'title': '股金中外',
-  'body': '該換檔喽咩噗',
+  'body': '唐唐生日快樂',
   'icon': 'https://firebasestorage.googleapis.com/v0/b/test-f3031.appspot.com/o/%E6%AD%B7%E5%8F%B2%E7%BE%8A.jpg?alt=media&token=cc95d21d-7aed-472d-911f-1bdeda3ecc11',
 };
 
