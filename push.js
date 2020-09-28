6f523c6d5c5a3776e8165a6906daaac55d4b0770
jean870809 <61371797+jean870809@users.noreply.github.com>
2020-09-28 14:05:59 +0800

    AAA

--- push.js
+++ push.js
@@ -3,10 +3,10 @@
 
 
 var key = 'AAAAsbNDVbs:APA91bG7xWF5l0EUl6UHLeOJyXUtL7AgzmGkw_F4BCoAxKJOZh_SVp7zhfypLaisu2_godUq31RWfTHiu-lzXkIsgvbrmWUBiPlWEDiRCZiPvieZjeiWVp823py053b2z5uJJpUkwJne';
-var to = "e-nOK15Pd0LNM5ySXhjiSu:APA91bFpPkhgluLXKBBweGabTj_ahuVGfJESTgxbwUvU3umz4u3mS0tAYEUv-kUdNsDzV3ljIqMN3y1Nk4DSeKAWAutbSvI53a0eR7U0F6jQfwZBKWl1ePpAKpMHtK5JO0AOgZsom0kB";
+var to = "dZG4J0imoiJsSFfvVXiP69:APA91bF7M9R1hUW1jbao2YYu3EgR9P5acfftpptsBIg2ip9edX3o_hdNBov4l063Jq-NyetSS0BFD8W0YvarcKizHo-rCKJrejt9BsOAFXBCD7Dh_fC0GSR8qf1tFkPRnYPY9wpJ6qEP";
 var notification = {
   'title': '股金中外',
-  'body': '唐唐生日快樂',
+  'body': 'Test',
   'icon': 'https://firebasestorage.googleapis.com/v0/b/test-f3031.appspot.com/o/%E6%AD%B7%E5%8F%B2%E7%BE%8A.jpg?alt=media&token=cc95d21d-7aed-472d-911f-1bdeda3ecc11',
 };
 
@@ -25,3 +25,4 @@ window.fetch('https://fcm.googleapis.com/fcm/send', {
 }).catch(function(error) {
   console.error(error);
 })
+
